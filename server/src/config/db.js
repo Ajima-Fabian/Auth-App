24a0fe5562ecc088c4ee7356d645768e137ea1a6
Ajima-Fabian <ajimafabian18@gmail.com>
2026-09-23 15:04:55 +0100

    Improve MongoDB connection handling in db.js
    Refactor connectDB function to check connection state before attempting to connect to MongoDB.

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 import { env } from "./env.js";
 
-const connectDB = async() => {
-    try{
-       const mongoDB = await mongoose.connect(env.MONGO_URI)
-
-       console.log(`MongoB connection successful: ${mongoDB.connection.host}`)
-
-    } catch(err){
-        console.error('MongoDB connection failed:', err.message)
-        process.exit(1)
+const connectDB = async () => {
+    if (mongoose.connection.readyState === 1) {
+        return mongoose.connection;
     }
-}
 
-export default connectDB
+    try {
+        const mongoDB = await mongoose.connect(env.MONGO_URI);
+
+        console.log(
+            `MongoDB connection successful: ${mongoDB.connection.host}`
+        );
+
+        return mongoDB.connection;
+    } catch (err) {
+        console.error("MongoDB connection failed:", err.message);
+        throw err;
+    }
+};
+
+export default connectDB;
